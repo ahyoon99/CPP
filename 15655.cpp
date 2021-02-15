eeb5a594d6c1d7db72ff79b7351a3f2ae89f0e18
ahyoon <kimay9623@naver.com>
2021-02-15 21:33:19 +0900

    15655_N과M(6)

---
+++ 15655.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[9];
+int result[9];
+bool check[10001];
+
+void go(int index, int start, int N, int M) {
+	if (index == M) {
+		for (int i = 0; i < M; i++) {
+			cout << result[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	else {
+		for (int i = start; i < N; i++) {
+			if (check[num[i]]) {
+				continue;
+			}
+			else {
+				check[num[i]] = true;
+				result[index] = num[i];
+				go(index + 1, i, N, M);
+				check[num[i]] = false;
+			}
+			
+		}
+	}
+
+}
+
+int main() {
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> num[i];
+	}
+
+	sort(num, num + N);
+
+	go(0, 0, N, M);
+
+	return 0;
+}

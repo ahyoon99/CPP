196b71d52ce900f1b5d976f9c4a19bff82b226dd
ahyoon <kimay9623@naver.com>
2021-02-19 16:50:42 +0900

    11724_연결 요소의 개수(BFS)

--- 11724.cpp
+++ 11724.cpp
@@ -1,18 +1,90 @@
-#include <iostream>
+// 规过 1 :  DFS
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//vector <int> num[1001];
+//bool check[1001];
+//
+//void DFS(int x) {
+//	check[x] = true;
+//	//cout << x << " ";
+//	for (int i = 0; i < num[x].size(); i++) {
+//		int y = num[x][i];
+//		if (check[y] == false) {
+//			DFS(y);
+//		}
+//	}
+//}
+//
+//int main() {
+//
+//	int N, M;
+//	cin >> N >> M;
+//
+//	for (int i = 0; i < M; i++) {
+//		int a, b;
+//		cin >> a >> b;
+//		num[a].push_back(b);
+//		num[b].push_back(a);
+//	}
+//
+//	for (int i = 0; i <= N; i++) {
+//		sort(num[i].begin(), num[i].end());
+//	}
+//
+//	int tot_cnt = 0;
+//
+//	while (true) {
+//		int now_idx;
+//		bool is_check = true;
+//		for (int i = 1; i <= N; i++) {
+//			if (check[i] == false) {
+//				now_idx = i;
+//				is_check = false;
+//				break;
+//			}
+//		}
+//		if (!is_check) {
+//			DFS(now_idx);
+//			tot_cnt++;
+//		}
+//		else {
+//			break;
+//		}
+//	}
+//
+//	cout << tot_cnt << "\n";
+//
+//	return 0;
+//}
+
+// 规过 2 : BFS
+#include<iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
-vector <int> num[1001];
+vector<int>num[1001];
 bool check[1001];
 
-void DFS(int x) {
+void BFS(int x) {
+	queue <int> q;
+	q.push(x);
 	check[x] = true;
-	//cout << x << " ";
-	for (int i = 0; i < num[x].size(); i++) {
-		int y = num[x][i];
-		if (check[y] == false) {
-			DFS(y);
+
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		//cout << x << " ";
+		for (int i = 0; i < num[x].size(); i++) {
+			int y = num[x][i];
+			if (check[y] == false) {
+				q.push(y);
+				check[y] = true;
+			}
 		}
 	}
 }
@@ -22,35 +94,24 @@ int main() {
 	int N, M;
 	cin >> N >> M;
 
+	int tot_cnt = 0;
+
 	for (int i = 0; i < M; i++) {
 		int a, b;
 		cin >> a >> b;
+
 		num[a].push_back(b);
 		num[b].push_back(a);
 	}
 
-	for (int i = 0; i <= N; i++) {
+	for (int i = 1; i <= N; i++) {
 		sort(num[i].begin(), num[i].end());
 	}
 
-	int tot_cnt = 0;
-
-	while (true) {
-		int now_idx;
-		bool is_check = true;
-		for (int i = 1; i <= N; i++) {
-			if (check[i] == false) {
-				now_idx = i;
-				is_check = false;
-				break;
-			}
-		}
-		if (!is_check) {
-			DFS(now_idx);
+	for (int i = 1; i <= N; i++) {
+		if (check[i] == false) {
+			BFS(i);
 			tot_cnt++;
-		}
-		else {
-			break;
 		}
 	}
 

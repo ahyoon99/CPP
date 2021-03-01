7ce269313b1b825c0d9d863f1fcbd8033856a79f
ahyoon <kimay9623@naver.com>
2021-03-01 15:59:11 +0900

    11727_2xn 타일링 2(Bottom-Up)

---
+++ 11727.cpp
@@ -0,0 +1,23 @@
+// ¹æ¹ý 1 : Bottom-Up
+#include <iostream>
+using namespace std;
+
+int d[1001];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	d[1] = 1;
+	d[2] = 3;
+
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + 2 * d[i - 2]) % 10007;
+	}
+
+	cout << d[n] << "\n";
+
+	return 0;
+
+}

f9fb0f61208b83669b9661b1996e7ddb0fd0497e
ahyoon <kimay9623@naver.com>
2021-03-06 16:43:21 +0900

    2193_이친수(1차원 배열 사용, Bottom-Up)

--- 2193.cpp
+++ 2193.cpp
@@ -41,27 +41,50 @@
 //}
 
 // 방법 2 : Bottom-Up
+//#include <iostream>
+//using namespace std;
+//
+//long long d[91][2];
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//
+//	d[1][0] = 0;
+//	d[1][1] = 1;
+//	d[2][0] = 1;
+//	d[2][1] = 0;
+//
+//	for (int i = 3; i <= n; i++) {
+//		d[i][0] = d[i - 1][0] + d[i - 1][1];
+//		d[i][1] = d[i - 2][0] + d[i - 2][1];
+//	}
+//
+//	cout << d[n][0] + d[n][1] << "\n";
+//
+//	return 0;
+//}
+
+// 방법 3 : 1차원 배열 사용 , Bottom-Up
 #include <iostream>
 using namespace std;
 
-long long d[91][2];
+long long d[91];
 
 int main() {
 
 	int n;
 	cin >> n;
 
-	d[1][0] = 0;
-	d[1][1] = 1;
-	d[2][0] = 1;
-	d[2][1] = 0;
+	d[1] = 1;
+	d[2] = 1;
 
 	for (int i = 3; i <= n; i++) {
-		d[i][0] = d[i - 1][0] + d[i - 1][1];
-		d[i][1] = d[i - 2][0] + d[i - 2][1];
+		d[i] = d[i - 1] + d[i - 2];
 	}
 
-	cout << d[n][0] + d[n][1] << "\n";
+	cout << d[n] << "\n";
 
 	return 0;
 }

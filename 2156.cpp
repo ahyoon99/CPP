59b949cec0637dc92446ffbbc6528d8d845c907a
ahyoon <kimay9623@naver.com>
2021-03-08 15:51:12 +0900

    2156_포도주 시식(1차원 배열 사용, Bottom-Up)

--- 2156.cpp
+++ 2156.cpp
@@ -44,12 +44,43 @@
 //}
 
 // 방법 2 : Bottom-Up
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int wine[10001];
+//int d[10001][3];
+//
+//int main() {
+//
+//	int n;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) {
+//		cin >> wine[i];
+//	}
+//
+//	d[1][0] = 0;
+//	d[1][1] = wine[1];
+//	d[1][2] = 0;
+//
+//	for (int i = 2; i <= n; i++) {
+//		d[i][0] = max({ d[i - 1][0], d[i - 1][1], d[i - 1][2] });
+//		d[i][1] = d[i - 1][0] + wine[i];
+//		d[i][2] = d[i - 1][1] + wine[i];
+//	}
+//
+//	cout << max({ d[n][0], d[n][1],d[n][2] }) << "\n";
+//
+//	return 0;
+//}
+
+// 방법 3 : 1차원 배열 사용 , Bottom-Up
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int wine[10001];
-int d[10001][3];
+int d[10001];
 
 int main() {
 
@@ -59,17 +90,20 @@ int main() {
 		cin >> wine[i];
 	}
 
-	d[1][0] = 0;
-	d[1][1] = wine[1];
-	d[1][2] = 0;
+	d[1] = wine[1];
+	d[2] = wine[1] + wine[2];
 
-	for (int i = 2; i <= n; i++) {
-		d[i][0] = max({ d[i - 1][0], d[i - 1][1], d[i - 1][2] });
-		d[i][1] = d[i - 1][0] + wine[i];
-		d[i][2] = d[i - 1][1] + wine[i];
+	for (int i = 3; i <= n; i++) {
+		d[i] = d[i - 1];
+		if (d[i] < d[i - 2] + wine[i]) {
+			d[i] = d[i - 2] + wine[i];
+		}
+		if (d[i] < d[i - 3] + wine[i] + wine[i - 1]) {
+			d[i] = d[i - 3] + wine[i] + wine[i - 1];
+		}
 	}
 
-	cout << max({ d[n][0], d[n][1],d[n][2] }) << "\n";
+	cout << d[n] << "\n";
 
 	return 0;
 }
